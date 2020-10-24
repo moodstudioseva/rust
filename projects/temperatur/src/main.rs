a37fa3f43fbc5c823d270ed54c31919fd9ad8031
Eva <eva.haerich@protonmail.com>
2020-10-24 23:04:16 +0200

    finished temperatur

--- projects/temperatur/src/main.rs
+++ projects/temperatur/src/main.rs
@@ -1,26 +1,33 @@
 use std::io;
 
 fn main() {
-
     loop {
-        println!("Umrechnung Fahreneinheit in Celsius.");
-        println!("Gib eine Zahl ein:");
-        let mut zahl = String::new();
-        
-        
-    
-        io::stdin()
-            .read_line(&mut zahl)
-            .expect("Fehler beim Lesen der Zeile");
-        
-        let zahl: f32 = match zahl.trim().parse() {
+        println!("Umrechnung Fahrenheit in Celsius.");
+        println!("Fahrenheit zu Celsius [fc], Celsius zu Fahreneinheit [cf]");
+        let auswahl = input("Auswahl: ");
+        if auswahl != "fc" && auswahl != "cf" {
+            continue;
+        }
+        let eingabe = input("Gib eine Zahl ein: ");
+        let mut zahl: f32 = match eingabe.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-       
-        let zahl = (zahl - 32.0) * 5.0/9.0;
-
-        println!("Es sind {} Grad Celsius.", zahl);
+        if auswahl == "fc" {
+            zahl = (zahl - 32.0) * 5.0 / 9.0;
+            println!("Es sind {} Grad Celsius.", zahl);
+        } else if auswahl == "cf" {
+            zahl = zahl * 9.0 / 5.0 + 32.0;
+            println!("Es sind {} Fahrenheit.", zahl);
+        }
     }
-    
+}
+
+fn input(x: &str) -> String {
+    println!("{}", x);
+    let mut text = String::new();
+    io::stdin()
+        .read_line(&mut text)
+        .expect("Fehler beim Lesen der Zeile");
+    text.trim().to_string()
 }
